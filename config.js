eb880947b747c907b6d07aac21b49803dba7511c
socialwork.dn <socialwork.dn@gmail.com>
2026-07-11 15:16:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// =====================================================================
+// ตั้งค่า URL ของ Google Apps Script Web App (JSON API) ที่นี่ที่เดียว
+// หากมีการ Deploy เวอร์ชันใหม่และได้ URL ใหม่ ให้แก้ไขค่านี้เพียงจุดเดียว
+// =====================================================================
+var GAS_API_URL = "https://script.google.com/macros/s/AKfycbxfeqdGsDf1PfVnkvdbyPQfvr0i3DFDRC0quLrNYGUqunX46yb4zZ6tBG9OMGgNFBPWRw/exec";
